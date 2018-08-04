a535590790c446fe8b15ae0f2977d745aa98ca0d
Jan Müller <jan.rv@t-online.de>
2018-08-04 11:58:05 +0200

    remove redundant origin parameter

--- geometer/curve.py
+++ geometer/curve.py
@@ -8,11 +8,9 @@ from numpy.polynomial import polynomial as pl
 
 class AlgebraicCurve:
 
-    def __init__(self, poly, symbols=None, origin=None):
+    def __init__(self, poly, symbols=None):
         if symbols is None:
             symbols = sympy.symbols(["x" + str(i) for i in range(len(poly.shape))])
-
-        self.origin = origin or Point(*([0] * (len(symbols) - 1)))
 
         if isinstance(poly, np.ndarray):
             self.coeff = poly
@@ -52,40 +50,40 @@ class AlgebraicCurve:
         return f
 
     def tangent(self, at: Point):
-        dx = polyval((at - self.origin).array, pl.polyder(self.coeff, axis=0))
-        dy = polyval((at - self.origin).array, pl.polyder(self.coeff, axis=1))
-        dz = polyval((at - self.origin).array, pl.polyder(self.coeff, axis=2))
-        return Line(dx, dy, dz) + self.origin
+        dx = polyval(at.array, pl.polyder(self.coeff, axis=0))
+        dy = polyval(at.array, pl.polyder(self.coeff, axis=1))
+        dz = polyval(at.array, pl.polyder(self.coeff, axis=2))
+        return Line(dx, dy, dz)
 
     def contains(self, pt: Point):
-        return np.isclose(polyval((pt - self.origin).array, self.coeff), 0)
+        return np.isclose(polyval(pt.array, self.coeff), 0)
 
 
 class Conic(AlgebraicCurve):
 
-    def __init__(self, matrix, origin=Point(0,0)):
+    def __init__(self, matrix):
         self.array = np.array(matrix)
         m = sympy.Matrix(matrix)
         x, y, z = sympy.symbols("x y z")
-        super(Conic, self).__init__(sum(a*b for a,b in zip(m.dot([x, y, z]), [x, y, z])), symbols=[x,y,z], origin=origin)
+        super(Conic, self).__init__(sum(a*b for a,b in zip(m.dot([x, y, z]), [x, y, z])), symbols=[x,y,z])
 
     @classmethod
-    def from_points(cls, a, b, c, d, e, *, origin=Point(0,0)):
+    def from_points(cls, a, b, c, d, e):
         ace = np.linalg.det([a.array, c.array, e.array])
         bde = np.linalg.det([b.array, d.array, e.array])
         ade = np.linalg.det([a.array, d.array, e.array])
         bce = np.linalg.det([b.array, c.array, e.array])
         m = ace*bde*np.outer(np.cross(a.array, d.array), np.cross(b.array, c.array))\
             - ade*bce*np.outer(np.cross(a.array, c.array), np.cross(b.array, d.array))
-        return Conic(m+m.T, origin=origin)
+        return Conic(m+m.T)
 
     def tangent(self, at: Point):
-        return Line(self.array.dot((at - self.origin).array)) + self.origin
+        return Line(self.array.dot(at.array))
 
 
 class Circle(Conic):
 
     def __init__(self, center: Point, radius: float):
-        super(Circle, self).__init__([[1,0,0],
-                                      [0,1,0],
-                                      [0,0,-radius**2]], center)
+        super(Circle, self).__init__([[1,0,-center.array[0]],
+                                      [0,1,-center.array[1]],
+                                      [-center.array[0],-center.array[1],center.array[:-1].dot(center.array[:-1])-radius**2]])
