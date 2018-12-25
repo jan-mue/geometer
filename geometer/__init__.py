768742602e96855c6921be0bb34db13e58d29606
Jan Müller <jan.rv@t-online.de>
2018-12-25 02:03:10 +0200

    import version in init file

--- geometer/__init__.py
+++ geometer/__init__.py
@@ -4,3 +4,4 @@ from .curve import EllipticCurve, Conic, Circle
 from .transformation import Transformation, rotation, translation
 from .operators import crossratio, is_cocircular, is_perpendicular, is_collinear, is_concurrent, dist, angle
 from .manifold import Path, residue
+from .__version__ import __version__
