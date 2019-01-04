c4079d3ae0c7320c7d0dac26161b1e0f468dea67
Jan Müller <jan.rv@t-online.de>
2019-01-04 14:52:12 +0100

    removed shapes

--- geometer/__init__.py
+++ geometer/__init__.py
@@ -1,5 +1,4 @@
 from .point import Point, Line, Plane, join, meet,  I, J, infty, infty_plane
-from .shapes import Segment, Polygon, Triangle, Rectangle
 from .curve import AlgebraicCurve, Conic, Circle, Quadric
 from .transformation import Transformation, rotation, translation
 from .operators import crossratio, is_cocircular, is_perpendicular, is_collinear, is_concurrent, is_coplanar, dist, angle, angle_bisectors, harmonic_set
