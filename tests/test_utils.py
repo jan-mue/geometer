45247d4cc37a2414795a58bea307199420d88fba
Jan Müller <jan.rv@t-online.de>
2020-02-15 15:03:34 +0100

    added adjugate test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -20,6 +20,16 @@ def test_array_to_poly():
     assert np_array_to_poly(a, (x, y, z)) == 5*x**2 + 2*x*y + 3*y**2 + z**3
 
 
+def test_adjugate():
+    a = np.array([[2, 3, 4, 3],
+                  [5, 1, 7, 6],
+                  [4, 7, 8, 1],
+                  [2, 3, 4, 5]])
+
+    adj = adjugate(a)
+    assert np.allclose(np.linalg.inv(a)*np.linalg.det(a), adj)
+
+
 def test_null_space():
     a = np.array([[2, 3, 4, 3],
                   [5, 1, 7, 6],
