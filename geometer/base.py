added array kwargs to Tensor classes, disabled Tensor.__pow__ for power=1, allow addition and subtraction of points to quadrics, fix normalization error for tensors of exact dtype, use tolerance constants as defaults, added Tensor.dtype property, allow subtraction of points from polytopes

geometer/base.py
@@ -24,6 +24,8 @@ class Tensor:
     covariant : :obj:`bool` or :obj:`list` of :obj:`int`, optional
         If False, all indices are contravariant. If a list of indices indices is supplied, the specified indices of the
         array will be covariant indices and all others contravariant indices. By default all indices are covariant.
+    **kwargs
+        Additional keyword arguments for the constructor of the numpy array as defined in `numpy.array`.
 
     Attributes
     ----------
@@ -36,20 +38,20 @@ class Tensor:
 
     """
 
-    def __init__(self, *args, covariant=True):
+    def __init__(self, *args, covariant=True, **kwargs):
         if len(args) == 0:
             raise TypeError("At least one argument is required.")
 
         if len(args) == 1:
             if isinstance(args[0], Tensor):
-                self.array = args[0].array
+                self.array = np.array(args[0].array, **kwargs)
                 self._covariant_indices = args[0]._covariant_indices
                 self._contravariant_indices = args[0]._contravariant_indices
                 return
             else:
-                self.array = np.atleast_1d(args[0])
+                self.array = np.array(args[0], **kwargs)
         else:
-            self.array = np.array(args)
+            self.array = np.array(args, **kwargs)
 
         self.array = np.real_if_close(self.array)
 
@@ -74,6 +76,11 @@ class Tensor:
     def shape(self):
         """:obj:`tuple` of :obj:`int`: The shape of the underlying numpy array, same as ``self.array.shape``."""
         return self.array.shape
+
+    @property
+    def dtype(self):
+        """numpy.dtype: The dtype of the underlying numpy array, same as ``self.array.dtype``."""
+        return self.array.dtype
 
     @property
     def tensor_shape(self):
@@ -175,7 +182,7 @@ class Tensor:
         return TensorDiagram((self, other)).calculate()
 
     def __pow__(self, power, modulo=None):
-        if modulo is not None or not isinstance(power, int) or power < 0:
+        if modulo is not None or not isinstance(power, int) or power < 1:
             return NotImplemented
 
         if power == 1:
@@ -188,7 +195,7 @@ class Tensor:
             d.add_edge(cur, prev)
             prev = cur
 
-        return Tensor(d.calculate())
+        return d.calculate()
 
     def __truediv__(self, other):
         if np.isscalar(other):
@@ -196,15 +203,17 @@ class Tensor:
         return NotImplemented
 
     def __add__(self, other):
-        other = Tensor(other)
-        return Tensor(self.array + other.array, covariant=self._covariant_indices)
+        if isinstance(other, Tensor):
+            other = other.array
+        return Tensor(self.array + other, covariant=self._covariant_indices)
 
     def __radd__(self, other):
         return self + other
 
     def __sub__(self, other):
-        other = Tensor(other)
-        return Tensor(self.array - other.array, covariant=self._covariant_indices)
+        if isinstance(other, Tensor):
+            other = other.array
+        return Tensor(self.array - other, covariant=self._covariant_indices)
 
     def __rsub__(self, other):
         return -self + other
@@ -259,7 +268,7 @@ class LeviCivitaTensor(Tensor):
             array[tuple(indices)] = np.prod(diff, axis=0)
 
             self._cache[size] = array
-        super(LeviCivitaTensor, self).__init__(array, covariant=bool(covariant))
+        super(LeviCivitaTensor, self).__init__(array, covariant=bool(covariant), copy=False)
 
 
 class KroneckerDelta(Tensor):
@@ -311,7 +320,7 @@ class KroneckerDelta(Tensor):
             f = np.vectorize(calc)
             array = np.fromfunction(f, tuple(2*p*[n]), dtype=int)
 
-        super(KroneckerDelta, self).__init__(array, covariant=range(p))
+        super(KroneckerDelta, self).__init__(array, covariant=range(p), copy=False)
 
 
 class TensorDiagram:
@@ -442,7 +451,7 @@ class TensorDiagram:
 
         temp = np.einsum(*args, result_indices[0] + result_indices[1])
 
-        return Tensor(temp, covariant=range(len(result_indices[0])))
+        return Tensor(temp, covariant=range(len(result_indices[0])), copy=False)
 
     def copy(self):
         result = TensorDiagram()

geometer/curve.py
@@ -32,10 +32,19 @@ class Quadric(ProjectiveElement):
 
     """
 
-    def __init__(self, matrix, is_dual=False):
+    def __init__(self, matrix, is_dual=False, **kwargs):
         self.is_dual = is_dual
-        matrix = matrix.array if isinstance(matrix, Tensor) else np.array(matrix)
-        super(Quadric, self).__init__(matrix, covariant=False)
+        kwargs.setdefault('covariant', False)
+        super(Quadric, self).__init__(matrix, **kwargs)
+
+    def __add__(self, other):
+        if not isinstance(other, Point):
+            return super(Quadric, self).__add__(other)
+
+        return translation(other) * self
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def __apply__(self, transformation):
         inv = transformation.inverse().array
@@ -59,7 +68,7 @@ class Quadric(ProjectiveElement):
 
         """
         m = np.outer(e.array, f.array)
-        return cls(m + m.T)
+        return cls(m + m.T, copy=False)
 
     def tangent(self, at):
         """Returns the hyperplane defining the tangent space at a given point.
@@ -93,7 +102,7 @@ class Quadric(ProjectiveElement):
         """
         return self.dual.contains(plane)
 
-    def contains(self, other, tol=1e-8):
+    def contains(self, other, tol=EQ_TOL_ABS):
         """Tests if a given point lies on the quadric.
 
         Parameters
@@ -206,7 +215,7 @@ class Quadric(ProjectiveElement):
     @property
     def dual(self):
         """Quadric: The dual quadric."""
-        return Quadric(np.linalg.inv(self.array), is_dual=not self.is_dual)
+        return Quadric(np.linalg.inv(self.array), is_dual=not self.is_dual, copy=False)
 
 
 class Conic(Quadric):
@@ -234,7 +243,7 @@ class Conic(Quadric):
         ade = np.linalg.det([a, d, e])
         bce = np.linalg.det([b, c, e])
         m = ace*bde*np.outer(np.cross(a, d), np.cross(b, c)) - ade*bce*np.outer(np.cross(a, c), np.cross(b, d))
-        return cls(m+m.T)
+        return cls(m+m.T, copy=False)
 
     @classmethod
     def from_lines(cls, g, h):
@@ -252,7 +261,7 @@ class Conic(Quadric):
 
         """
         m = np.outer(g.array, h.array)
-        return cls(m + m.T)
+        return cls(m + m.T, copy=False)
 
     @classmethod
     def from_tangent(cls, tangent, a, b, c, d):
@@ -313,7 +322,7 @@ class Conic(Quadric):
         """
         t1, t2, t3, t4 = Line(f1, I), Line(f1, J), Line(f2, I), Line(f2, J)
         c = cls.from_tangent(Line(bound.array), Point(t1.array), Point(t2.array), Point(t3.array), Point(t4.array))
-        return cls(np.linalg.inv(c.array))
+        return cls(np.linalg.inv(c.array), copy=False)
 
     @classmethod
     def from_crossratio(cls, cr, a, b, c, d):
@@ -347,7 +356,7 @@ class Conic(Quadric):
 
         matrix = np.outer(ac, bd) - cr * np.outer(ad, bc)
 
-        return cls(matrix + matrix.T)
+        return cls(matrix + matrix.T, copy=False)
 
     def intersect(self, other):
         """Calculates points of intersection with the conic.
@@ -460,7 +469,7 @@ class Ellipse(Conic):
 
     """
 
-    def __init__(self, center=Point(0, 0), hradius=1, vradius=1):
+    def __init__(self, center=Point(0, 0), hradius=1, vradius=1, **kwargs):
         r = np.array([vradius ** 2, hradius ** 2, 1])
         c = -center.normalized_array
         d = c * r
@@ -469,7 +478,8 @@ class Ellipse(Conic):
         m[2, :] = d
         m[:, 2] = d
         m[2, 2] = d.dot(c) - (r[0] * r[1] + 1)
-        super(Ellipse, self).__init__(m)
+        kwargs['copy'] = False
+        super(Ellipse, self).__init__(m, **kwargs)
 
 
 class Circle(Ellipse):
@@ -484,8 +494,8 @@ class Circle(Ellipse):
 
     """
 
-    def __init__(self, center=Point(0, 0), radius=1):
-        super(Circle, self).__init__(center, radius, radius)
+    def __init__(self, center=Point(0, 0), radius=1, **kwargs):
+        super(Circle, self).__init__(center, radius, radius, **kwargs)
 
     @property
     def center(self):
@@ -547,13 +557,14 @@ class Sphere(Quadric):
 
     """
 
-    def __init__(self, center=Point(0, 0, 0), radius=1):
+    def __init__(self, center=Point(0, 0, 0), radius=1, **kwargs):
         c = -center.normalized_array
         m = np.eye(center.shape[0], dtype=np.find_common_type([c.dtype, type(radius)], []))
         m[-1, :] = c
         m[:, -1] = c
         m[-1, -1] = c[:-1].dot(c[:-1])-radius**2
-        super(Sphere, self).__init__(m)
+        kwargs['copy'] = False
+        super(Sphere, self).__init__(m, **kwargs)
 
     @property
     def center(self):
@@ -597,7 +608,7 @@ class Cone(Quadric):
 
     """
 
-    def __init__(self, vertex=Point(0, 0, 0), base_center=Point(0, 0, 1), radius=1):
+    def __init__(self, vertex=Point(0, 0, 0), base_center=Point(0, 0, 1), radius=1, **kwargs):
         from .operators import dist, angle
 
         h = dist(vertex, base_center)
@@ -632,7 +643,8 @@ class Cone(Quadric):
             t = translation(Point(v)) * t * translation(-Point(v))
             m = t.array.T.dot(m).dot(t.array)
 
-        super(Cone, self).__init__(m)
+        kwargs['copy'] = False
+        super(Cone, self).__init__(m, **kwargs)
 
 
 class Cylinder(Cone):
@@ -649,6 +661,6 @@ class Cylinder(Cone):
 
     """
 
-    def __init__(self, center=Point(0, 0, 0), direction=Point(0, 0, 1), radius=1):
+    def __init__(self, center=Point(0, 0, 0), direction=Point(0, 0, 1), radius=1, **kwargs):
         vertex = infty_plane.meet(Line(center, center+direction))
-        super(Cylinder, self).__init__(vertex, center, radius)
+        super(Cylinder, self).__init__(vertex, center, radius, **kwargs)

geometer/operators.py
@@ -1,7 +1,7 @@
 import numpy as np
 from .point import Point, Line, Plane, I, J, infty, infty_plane, join, meet
 from .curve import absolute_conic
-from .base import LeviCivitaTensor, TensorDiagram
+from .base import LeviCivitaTensor, TensorDiagram, EQ_TOL_REL, EQ_TOL_ABS
 from .exceptions import IncidenceError, NotCollinear
 from .utils import orth
 
@@ -265,7 +265,7 @@ def dist(p, q):
         return 4*abs(np.sqrt(pqi * pqj)/(pij*qij))
 
 
-def is_cocircular(a, b, c, d, rtol=1.e-5, atol=1.e-8):
+def is_cocircular(a, b, c, d, rtol=EQ_TOL_REL, atol=EQ_TOL_ABS):
     """Tests whether four points lie on a circle.
 
     Parameters
@@ -299,7 +299,7 @@ def is_cocircular(a, b, c, d, rtol=1.e-5, atol=1.e-8):
     return np.isclose(i, j, rtol, atol)
 
 
-def is_perpendicular(l, m, rtol=1.e-5, atol=1.e-8):
+def is_perpendicular(l, m, rtol=EQ_TOL_REL, atol=EQ_TOL_ABS):
     """Tests whether two lines/planes are perpendicular.
 
     Parameters
@@ -343,7 +343,7 @@ def is_perpendicular(l, m, rtol=1.e-5, atol=1.e-8):
     return np.isclose(crossratio(L, M, I, J, Point(1, 1)), -1, rtol, atol)
 
 
-def is_coplanar(*args, tol=1.e-8):
+def is_coplanar(*args, tol=EQ_TOL_ABS):
     """Tests whether the given points or lines are collinear, coplanar or concurrent. Works in any dimension.
 
     Due to line point duality this function has dual versions :obj:`is_collinear`, :obj:`is_collinear` and

geometer/point.py
@@ -43,10 +43,10 @@ def _join_meet_duality(*args, intersect_lines=True):
                 i = np.unravel_index(np.abs(array).argmax(), array.shape)
                 if not intersect_lines:
                     # extract the common subspace
-                    result = Tensor(array[i[0], ...], covariant=False)
+                    result = Tensor(array[i[0], ...], covariant=False, copy=False)
                 else:
                     # extract the point of intersection
-                    result = Tensor(array[(slice(None),) + i[1:]])
+                    result = Tensor(array[(slice(None),) + i[1:]], copy=False)
 
             elif intersect_lines or n == 4:
                 # can't intersect lines that are not coplanar and can't join skew lines in 3D
@@ -63,18 +63,18 @@ def _join_meet_duality(*args, intersect_lines=True):
         raise LinearDependenceError("Arguments are not linearly independent.")
 
     # normalize result to avoid large values
-    result /= np.max(np.abs(result.array))
+    result.array = result.array / np.max(np.abs(result.array))
 
     if result.tensor_shape == (0, 1):
-        return Line(result) if n == 3 else Plane(result)
+        return Line(result, copy=False) if n == 3 else Plane(result, copy=False)
     if result.tensor_shape == (1, 0):
-        return Point(result)
+        return Point(result, copy=False)
     if result.tensor_shape == (2, 0):
-        return Line(result).contravariant_tensor
+        return Line(result, copy=False).contravariant_tensor
     if result.tensor_shape == (0, n-2):
-        return Line(result)
+        return Line(result, copy=False)
 
-    return Subspace(result)
+    return Subspace(result, copy=False)
 
 
 def join(*args):
@@ -128,11 +128,11 @@ class Point(ProjectiveElement):
 
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if np.isscalar(args[0]):
-            super(Point, self).__init__(*args, 1)
+            super(Point, self).__init__(*args, 1, **kwargs)
         else:
-            super(Point, self).__init__(*args)
+            super(Point, self).__init__(*args, **kwargs)
 
     def __add__(self, other):
         if not isinstance(other, Point):
@@ -222,8 +222,9 @@ class Subspace(ProjectiveElement):
 
     """
 
-    def __init__(self, *args):
-        super(Subspace, self).__init__(*args, covariant=False)
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('covariant', False)
+        super(Subspace, self).__init__(*args, **kwargs)
 
     def __add__(self, other):
         if not isinstance(other, Point):
@@ -263,7 +264,7 @@ class Subspace(ProjectiveElement):
             if not self.contains(p):
                 return p
 
-    def contains(self, other, tol=1e-8):
+    def contains(self, other, tol=EQ_TOL_ABS):
         """Tests whether a given point or line lies in the subspace.
 
         Parameters
@@ -377,12 +378,12 @@ class Line(Subspace):
 
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) == 2:
-            pt1, pt2 = args
-            super(Line, self).__init__(pt1.join(pt2))
+            kwargs['copy'] = False
+            super(Line, self).__init__(join(*args), **kwargs)
         else:
-            super(Line, self).__init__(*args)
+            super(Line, self).__init__(*args, **kwargs)
 
     @property
     def covariant_tensor(self):
@@ -575,18 +576,19 @@ infty = Line(0, 0, 1)
 
 class Plane(Subspace):
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if all(isinstance(o, (Line, Point)) for o in args):
-            super(Plane, self).__init__(join(*args))
+            kwargs['copy'] = False
+            super(Plane, self).__init__(join(*args), **kwargs)
         else:
-            super(Plane, self).__init__(*args)
+            super(Plane, self).__init__(*args, **kwargs)
 
     @property
     def basis_matrix(self):
         """numpy.ndarray: A matrix with orthonormal basis vectors as rows."""
         n = self.dim + 1
         i = np.where(self.array != 0)[0][0]
-        result = np.zeros((n, n - 1), dtype=self.array.dtype)
+        result = np.zeros((n, n - 1), dtype=self.dtype)
         a = [j for j in range(n) if j != i]
         result[i, :] = self.array[a]
         result[a, range(n - 1)] = -self.array[i]

geometer/shapes.py
@@ -27,14 +27,14 @@ class Polytope(Tensor):
 
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) > 1:
             args = tuple(a.array for a in args)
         super(Polytope, self).__init__(*args, covariant=[-1])
 
     def __apply__(self, transformation):
         result = self.copy()
-        result.array = self.array.dot(transformation.array.T)
+        result.array = self.array @ transformation.array.T
         return result
 
     def __repr__(self):
@@ -106,6 +106,9 @@ class Polytope(Tensor):
             return type(self)(*[f + other for f in self.facets])
         return super(Polytope, self).__add__(other)
 
+    def __sub__(self, other):
+        return self + (-other)
+
 
 class Segment(Polytope):
     """Represents a line segment in an arbitrary projective space.
@@ -127,8 +130,8 @@ class Segment(Polytope):
 
     """
 
-    def __init__(self, *args):
-        super(Segment, self).__init__(*args)
+    def __init__(self, *args, **kwargs):
+        super(Segment, self).__init__(*args, **kwargs)
         self._line = Line(Point(self.array[0]), Point(self.array[1]))
 
     def __apply__(self, transformation):
@@ -136,7 +139,7 @@ class Segment(Polytope):
         result._line = Line(Point(result.array[0]), Point(result.array[1]))
         return result
 
-    def contains(self, other, tol=1e-8):
+    def contains(self, other, tol=EQ_TOL_ABS):
         """Tests whether a point is contained in the segment.
 
         Parameters
@@ -223,16 +226,16 @@ class Simplex(Polytope):
 
     """
 
-    def __new__(cls, *args):
+    def __new__(cls, *args, **kwargs):
         if len(args) == 2:
-            return Segment(*args)
+            return Segment(*args, **kwargs)
 
         return super(Polytope, cls).__new__(cls)
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) > 3:
             args = [Simplex(*x) for x in combinations(args, len(args)-1)]
-        super(Simplex, self).__init__(*args)
+        super(Simplex, self).__init__(*args, **kwargs)
 
     @property
     def volume(self):
@@ -266,10 +269,10 @@ class Polygon(Polytope):
 
     """
 
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if all(isinstance(x, Segment) for x in args):
             args = (np.array([s.array[0] for s in args]),)
-        super(Polygon, self).__init__(*args)
+        super(Polygon, self).__init__(*args, **kwargs)
         self._plane = Plane(*self.vertices[:self.dim]) if self.dim > 2 else None
 
     def __apply__(self, transformation):
@@ -420,7 +423,7 @@ class RegularPolygon(Polygon):
 
     """
 
-    def __init__(self, center, radius, n, axis=None):
+    def __init__(self, center, radius, n, axis=None, **kwargs):
         if axis is None:
             p = Point(1, 0)
         else:
@@ -435,7 +438,7 @@ class RegularPolygon(Polygon):
             t = translation(center) * t * translation(-center)
             vertices.append(t*vertex)
 
-        super(RegularPolygon, self).__init__(*vertices)
+        super(RegularPolygon, self).__init__(*vertices, **kwargs)
 
     @property
     def radius(self):
@@ -533,9 +536,9 @@ class Cuboid(Polyhedron):
 
     """
 
-    def __init__(self, a, b, c, d):
+    def __init__(self, a, b, c, d, **kwargs):
         x, y, z = b-a, c-a, d-a
         yz = Rectangle(a, a + z, a + y + z, a + y)
         xz = Rectangle(a, a + x, a + x + z, a + z)
         xy = Rectangle(a, a + x, a + x + y, a + y)
-        super(Cuboid, self).__init__(yz, xz, xy, yz + x, xz + y, xy + z)
+        super(Cuboid, self).__init__(yz, xz, xy, yz + x, xz + y, xy + z, **kwargs)