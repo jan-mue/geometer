25a5fc8ef08a3159e4f126f286a77d8745e9f9f7
Jan Müller <jan.rv@t-online.de>
2021-06-27 17:50:51 +0200

    update dependencies

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ REQUIRES_PYTHON = ">=3.5.0"
 VERSION = None
 
 # What packages are required for this module to be executed?
-REQUIRED = ["numpy>=1.15,<1.21"]
+REQUIRED = ["numpy>=1.15,<1.22"]
 
 # What packages are optional?
 EXTRAS = {
