fix numerical instability

geometer/curve.py
@@ -79,7 +79,8 @@ class Quadric(ProjectiveTensor):
 
         """
         m = outer(e.array, f.array)
-        return Quadric(m + m.T, normalize_matrix=True)
+        m += m.T
+        return Quadric(m, normalize_matrix=True)
 
     def tangent(self, at: Point) -> Plane:
         """Returns the hyperplane defining the tangent space at a given point.
@@ -296,7 +297,8 @@ class Conic(Quadric):
 
         """
         m = outer(g.array, h.array)
-        return Conic(m + m.T, normalize_matrix=True)
+        m += m.T
+        return Conic(m, normalize_matrix=True)
 
     @classmethod
     def from_tangent(cls, tangent: Line, a: Point, b: Point, c: Point, d: Point) -> Conic:
@@ -395,8 +397,9 @@ class Conic(Quadric):
         bc = adjugate([np.ones(3), b.array, c.array])[:, 0]
 
         matrix = outer(ac, bd) - cr * outer(ad, bc)
+        matrix += matrix.T
 
-        return cls(matrix + matrix.T, normalize_matrix=True)
+        return cls(matrix, normalize_matrix=True)
 
     def intersect(self, other: Line | Conic) -> list[Point]:
         """Calculates points of intersection with the conic.

geometer/point.py
@@ -8,7 +8,7 @@ from typing_extensions import Literal
 
 from .base import EQ_TOL_ABS, LeviCivitaTensor, ProjectiveTensor, Tensor, TensorDiagram, TensorIndex
 from .exceptions import GeometryException, LinearDependenceError, NotCoplanar
-from .utils import matmul, matvec, null_space
+from .utils import matmul, matvec, null_space, reduce_multiples
 
 
 @overload
@@ -120,12 +120,7 @@ def _join_meet_duality(
 
     if normalize_result:
         axes = tuple(result._covariant_indices) + tuple(result._contravariant_indices)
-        max_abs = np.max(np.abs(result.array), axis=axes, keepdims=True)
-        if check_dependence:
-            result.array = result.array / max_abs
-        else:
-            with np.errstate(divide="ignore", invalid="ignore"):
-                result.array = result.array / max_abs
+        reduce_multiples(result.array, axis=axes, out=result.array)
 
     if result.tensor_shape == (0, 1):
         return Line(result, copy=False) if n == 3 else Plane(result, copy=False)

geometer/utils/math.py
@@ -450,7 +450,8 @@ def roots(p: npt.ArrayLike) -> npt.NDArray[np.float_ | np.complex_]:
         return np.array([x1, x2, x3])
 
 
-def outer(a: npt.ArrayLike, b: npt.ArrayLike) -> npt.NDArray[np.int_ | np.float_ | np.complex_]:
+def outer(a: npt.ArrayLike, b: npt.ArrayLike,
+          out: np.ndarray | None = None) -> npt.NDArray[np.int_ | np.float_ | np.complex_]:
     """Outer product of two arrays.
 
     Parameters
@@ -469,4 +470,55 @@ def outer(a: npt.ArrayLike, b: npt.ArrayLike) -> npt.NDArray[np.int_ | np.float_
 
     """
     a, b = np.asarray(a), np.asarray(b)
-    return np.multiply(a[..., None], b[..., None, :])
+    return np.multiply(a[..., None], b[..., None, :], out=out)
+
+
+def reduce_multiples(a: npt.ArrayLike, axis: tuple[int, ...] | int | None = None,
+                     out: np.ndarray | None = None) -> np.ndarray:
+    """Normalize the input array by dividing by the largest common divisor (for integer data types)
+    or the largest power of two used in the internal floating point representation (for float and complex data types).
+
+    Parameters
+    ----------
+    a : array_like
+        The array to normalize.
+    axis : None or int or tuple of ints, optional
+        The axis along which the common divisor is chosen.
+    out : ndarray or None, optional
+        The array to output the results to. By default, a new array will be returned.
+
+    Returns
+    -------
+    numpy.ndarray
+        The normalized array.
+
+    """
+    a = np.asarray(a)
+
+    if a.dtype.kind == "b":
+        return a
+
+    if a.dtype.kind == "f":
+        mantissa, exponent = np.frexp(a)
+        exponent -= np.max(exponent, axis=axis, keepdims=True)
+        return np.ldexp(mantissa, exponent, dtype=a.dtype, out=out)
+
+    if a.dtype.kind == "c":
+        rm, re = np.frexp(a.real)
+        im, ie = np.frexp(a.imag)
+        max_re = np.max(re, axis=axis, keepdims=True)
+        max_ie = np.max(ie, axis=axis, keepdims=True)
+        max_exponent = np.maximum(max_re, max_ie)
+        re -= max_exponent
+        ie -= max_exponent
+        if out is None:
+            out = np.empty_like(a)
+        np.ldexp(rm, re, out=out.real)
+        np.ldexp(im, ie, out=out.imag)
+        return out
+
+    if a.dtype.kind in "iu":
+        gcd = np.gcd.reduce(a, axis=axis, keepdims=True)
+        return np.floor_divide(a, gcd, out=out)
+
+    raise NotImplementedError(f"Array with dtype {a.dtype} cannot be reduced.")